8a6ad58f388315daf43008c03b43919b30ebb9a2
DKT <b.apanasewicz@gmail.com>
2020-04-06 11:50:39 +0200

    look

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark height="80px">
+    <v-app-bar app color="primary" dark height="100px">
       <div class="d-none d-sm-flex align-center">
-        <v-img alt="Samar logo" class="mr-1" contain src="sign.png" width="30" />
-        <div class="title mr-5">SIDCLOUD</div>
+        <v-img alt="Samar logo" class="mr-1" contain src="sign.png" width="40" />
+        <v-flex class="font-weight-thin headline mr-5">SIDCLOUD</v-flex>
       </div>
 
       <v-btn @click="click('play',last_index)" class="mr-1" fab small>
@@ -20,7 +20,7 @@
       <v-btn @click="click('jmp',last_index)" class="mr-1" fab small>
         <v-icon>replay</v-icon>
       </v-btn>
-      <v-btn @click="click('stop',last_index)" fab small>
+      <v-btn @click="click('stop',last_index)" class="mr-2" fab small>
         <v-icon>stop</v-icon>
       </v-btn>
       <!-- <v-btn class="mr-2" fab small> -->
@@ -30,7 +30,7 @@
 
       <v-spacer></v-spacer>
       <div class="hidden-xs-and-down align-center">
-        <div class="subtitle">{{title_playing}}</div>
+        <div class="font-weight-thin headline">{{title_playing}}</div>
       </div>
 
       <v-spacer></v-spacer>
@@ -48,7 +48,7 @@
           <v-col v-for="(card,index) in releases" :key="card.ReleaseID">
             <v-card @click="click('jmp',index)" class="mx-auto mb-5" max-width="384">
               <v-img :src="card.ReleaseScreenShot" width="384" height="272"></v-img>
-              <v-card-title v-text="card.ReleaseName.substring(0, 35)"></v-card-title>
+              <v-card-title v-text="card.ReleaseName.substring(0, 31)"></v-card-title>
               <v-card-subtitle>
                 <v-div
                   v-for="(by,index) in card.ReleasedBy"
@@ -64,8 +64,8 @@
               </v-card-text>
             </v-card>
             <!-- <v-btn href="https://csdb.dk/release/?id=" target="_blank" text> -->
-              <!-- <span class="mr-2">CSDb</span> -->
-              <!-- <v-icon>link</v-icon> -->
+            <!-- <span class="mr-2">CSDb</span> -->
+            <!-- <v-icon>link</v-icon> -->
             <!-- </v-btn> -->
           </v-col>
         </v-row>
@@ -88,6 +88,7 @@ export default {
     title_playing: "",
     audio_url: "",
     play: false,
+    paused: false,
     player_type: "sidplayfp",
     last_index: 0
   }),
@@ -110,7 +111,7 @@ export default {
     },
     AudioUrl() {
       // eslint-disable-next-line
-      console.log("Player type: ", this.player_type);
+      // console.log("Player type: ", this.player_type);
 
       if (
         axios.defaults.baseURL == null ||
@@ -123,23 +124,22 @@ export default {
       }
     },
     click(job, id) {
-      console.log("Clicked on " + id);
+      // console.log("Clicked on " + id);
       var player = document.getElementById("radio");
 
       if (job == "jmp") {
         if (id < 0 || id >= this.releases.length) {
-          console.log("last track");
+          // console.log("last track");
           return;
         }
       }
 
       if (job == "stop") {
-        if (this.play) {
-          player.pause();
-          player.currentTime = 0.0;
-          this.audio_url = "";
-          this.play = false;
-        }
+        player.pause();
+        player.currentTime = 0.0;
+        this.audio_url = "";
+        this.play = false;
+        this.paused = false;
       } else {
         if (!this.play || job == "jmp") {
           this.title_playing = this.releases[id].ReleaseName;
@@ -147,58 +147,77 @@ export default {
 
           var query =
             "/api/v1/audio?sid_url=" + this.releases[id].DownloadLinks[0];
-          console.log("Query = " + query);
+          // console.log("Query = " + query);
 
           player.addEventListener("waiting", function() {
-            console.log("player event: waiting");
+            // console.log("player event: waiting");
           });
           player.addEventListener("play", function() {
-            console.log("player event: play");
+            // console.log("player event: play");
           });
           player.addEventListener("pause", function() {
-            console.log("player event: pause");
+            // console.log("player event: pause");
           });
           player.addEventListener("ended", function() {
-            console.log("player event: ended");
+            // console.log("player event: ended");
+            player.pause();
+            player.currentTime = 0.0;
+            this.audio_url = "";
+            this.play = false;
+            this.paused = false;
           });
           player.addEventListener("loadstart", function() {
-            console.log("player event: loadstart");
+            // console.log("player event: loadstart");
           });
           player.addEventListener("durationchange", function() {
-            console.log("player event: durationchange");
+            // console.log("player event: durationchange");
           });
           player.addEventListener("loadedmetadata", function() {
-            console.log("player event: loadedmetadata");
+            // console.log("player event: loadedmetadata");
           });
           player.addEventListener("loadeddata", function() {
-            console.log("player event: loadeddata");
+            // console.log("player event: loadeddata");
           });
           // player.addEventListener("progress", function() {
           //   console.log("player event: progress");
           // });
+          // TODO Dodać licznik czasu
           player.addEventListener("canplay", function() {
             console.log("player event: canplay");
             player.play();
             console.log("Playing...");
           });
           player.addEventListener("canplaythrough", function() {
-            console.log("player event: canplaythrough");
+            // console.log("player event: canplaythrough");
           });
 
-          player.pause();
-          player.currentTime = 0.0;
+          if (!this.paused || job == "jmp") {
+            player.pause();
+            player.currentTime = 0.0;
+            this.paused = false;
+            this.play = false;
 
-          this.AudioUrl();
+            this.AudioUrl();
 
-          axios.post(query).then(response => {
-            console.log("response " + response.data);
-
-            player.load();
-            console.log("Loading...");
+            axios.post(query).then(response => {
+              console.log(response.data);
+              player.load();
+              console.log("Loading...");
+              this.play = true;
+            });
+          } else {
+            player.pause();
+            this.paused = true;
+            this.play = false;
+          }
+          if (this.paused) {
+            player.play();
+            this.paused = false;
             this.play = true;
-          });
+          }
         } else {
           player.pause();
+          this.paused = true;
           this.play = false;
         }
       }
